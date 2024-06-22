00c49361ad535226fc3ccb6e681a62096e56ff07
devgiordane <bruno_giordane@outlook.com>
2024-06-22 17:24:51 -0300

    v1

---
+++ lipedema/app.vue
@@ -0,0 +1,187 @@
+<template>
+  <div class="container mx-auto">
+    <div class="hero min-h-screen" style="background-image: url(bg-lipedema-01.jpg);">
+      <div class="hero-content text-center text-neutral-content">
+        <div class="max-w-md">
+          <img src="assets/ipfbranca.png" class="pb-40 w-72 mx-auto">
+          <h1 class="mb-5 text-5xl font-serif text-yellow-100">Lipedema Clinic</h1>
+          <img src="assets/woman-lipedema.webp" class="pb-40 w-full rounded-lg mx-auto">
+        </div>
+      </div>
+    </div>
+
+
+    <div class="hero bg-base-200 py-10 ">
+      <div class="hero-content flex">
+        <img src="assets/patricia-01.jpg" class="max-w-sm rounded-lg shadow-2xl w-40 object-cover" />
+        <div>
+          <h1 class="text-3xl font-serif  ">Bem-vindo à Clínica de
+            Tratamento para Lipedema
+          </h1>
+
+          <p class="py-6 text-justify">Apresentamos um espaço dedicado ao tratamento e pesquisa do lipedema, liderado
+            pela Dra.
+            Patrícia Froes, renomada pesquisadora na área.</p>
+
+        </div>
+      </div>
+    </div>
+    <div class="bg-verde-50 py-10 text-white px-4 flex">
+      <div class="">
+        <h1 class="text-3xl font-serif  ">Nossa Missão
+        </h1>
+        <div class="w-0.5 bg-white h-20 mt-5 rounded-md  opacity-60"></div>
+        <span class="py-6">Promover a saúde e o bem-estar, oferecendo tratamentos inovadores e
+          personalizados
+          para pacientes com lipedema, aliando pesquisa científica e prática clínica.</span>
+      </div>
+      <img src="assets/img-01.png" class="w-40 rounded-xl" alt="">
+    </div>
+    <div class="bg-verde-50 py-10 text-white px-4">
+
+      <h1 class="text-3xl font-serif  ">Equipe de Profissionais
+
+      </h1>
+      <h3 class="font-light text-xl py-5">Conheça nossa equipe
+        multidisciplinar, composta por
+        profissionais renomados e
+        especializados no tratamento do
+        lipedema</h3>
+      <div class="flex pt-10 gap-3">
+        <img src="assets/patricia-02.jpg" class="max-w-sm rounded-lg shadow-2xl w-40 object-cover" />
+        <div>
+          <h1 class="text-3xl font-serif  ">DRA. PATRÍCIA FROES
+
+          </h1>
+          <p>FISIOTERAPEUTA</p>
+          <p class="text-xl py-2">Professora Doutora</p>
+          <p class="text-justify "> Leciona e orienta no
+            campo da fisioterapia, com ênfase em
+            tratamentos dermatofuncionais e lipedema.
+          </p>
+          <p class="text-xl py-2">Coordenadora de Pesquisa e Inovação</p>
+          <p>
+
+            Lidera
+            projetos inovadores para o desenvolvimento
+            de novas técnicas e tratamentos para o
+            lipedema.</p>
+
+        </div>
+      </div>
+      <div class="flex pt-10 gap-3">
+        <div>
+          <h1 class="text-3xl font-serif  ">ALESSANDRA NÓBREGA
+          </h1>
+          <p>FISIOTERAPEUTA</p>
+          <p class="text-xl py-2">Foco em Lipedema</p>
+          <p class="text-justify "> Responsável pelo
+            atendimento direcionado a pacientes com
+            lipedema da Clínica.
+          </p>
+          <p class="text-xl py-2">Especialização em Fisioterapia Vascular e
+            Dermatofuncional</p>
+
+
+        </div>
+        <img src="assets/alessandra-01.jpg" class="max-w-sm rounded-lg shadow-2xl w-40 object-cover" />
+      </div>
+      <div class="flex pt-10 gap-3">
+        <img src="assets/patricia-02.jpg" class="max-w-sm rounded-lg shadow-2xl w-40 object-cover" />
+        <div>
+          <h1 class="text-3xl font-serif  ">KÁTIA VIANA
+
+
+          </h1>
+          <p>NUTRICIONISTA CLÍNICA</p>
+          <p class="text-xl py-2">Foco em Lipedema</p>
+          <p class="text-justify "> Responsável pelo
+            Atendimento Nutricional da Clínica
+          </p>
+          <p class="text-xl py-2">Experiência Clínica e Acadêmica</p>
+          <p>
+
+            mestre em ciencias da saúde, vinte anos de
+            experiência clínica e docência no ensino
+            superior.</p>
+          <p class="text-xl py-2">Especializações</p>
+          <p>
+
+            Nutrição Clínica Funcional, Fitoterapia
+            e Suplementação Magistral</p>
+          <p class="text-xl py-2">Expertise em Tratamento Modulador da
+            Inflamação</p>
+
+        </div>
+      </div>
+
+    </div>
+    <div class="text-center ">
+      <h1 class="text-5xl font-serif py-10  ">Protocolo de Tratamento conservador
+      </h1>
+      <h3 class="font-medium text-2xl py-5">Tratamento Integrado de Lipedema
+      </h3>
+      <ul class="timeline timeline-vertical">
+        <li>
+          <div class="timeline-middle">
+            <CircleDot />
+          </div>
+          <div class="timeline-end">
+
+            <div class="text-lg font-black">Terapias Nutricionais com base na
+              avaliação nutricional global</div>
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div class="timeline-middle">
+            <CircleDot />
+          </div>
+          <div class="timeline-start mb-10">
+            <div class="text-lg font-black">Planejamento Dietético AntiInflamatório</div>
+            Foco: Dieta rica em alimentos antiinflamatório com Objetivo de Reduzir a
+            inflamação sistêmica que podem agravar
+            os sintomas do lipedema.
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div class="timeline-middle">
+            <CircleDot />
+          </div>
+          <div class="timeline-end mb-10">
+            <div class="text-lg font-black">Suplementação Estratégica e Fitoterapia
+            </div>
+            Objetivo: Complementar a dieta e
+            maximizar o controle da inflamação.
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div class="timeline-middle">
+            <CircleDot />
+          </div>
+          <div class="timeline-start mb-10">
+            <div class="text-lg font-black">Educação Nutricional com foco
+              Comportamental
+            </div>
+            Objetivo: Capacitar os pacientes a fazer
+            escolhas alimentares que favoreça a
+            longo prazo o controle dos sinais e
+            sintomas do lipedema.
+          </div>
+
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import { CircleDot } from 'lucide-vue-next';
+
+</script>
---
+++ lipedema/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        verde: {
+          50: "#0a4049",
+        },
+      },
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["corporate"],
+  },
+};
